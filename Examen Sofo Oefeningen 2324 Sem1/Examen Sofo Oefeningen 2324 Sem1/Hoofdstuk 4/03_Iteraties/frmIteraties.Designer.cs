a791b04a496c3154e7e33b1388bdb3f75fa1838e
professorVerbrugghe <lotrdetailsys@gmail.com>
2023-12-05 14:13:08 +0100

    Update frmIteraties.Designer.cs

--- Examen Sofo Oefeningen 2324 Sem1/Examen Sofo Oefeningen 2324 Sem1/Hoofdstuk 4/03_Iteraties/frmIteraties.Designer.cs
+++ Examen Sofo Oefeningen 2324 Sem1/Examen Sofo Oefeningen 2324 Sem1/Hoofdstuk 4/03_Iteraties/frmIteraties.Designer.cs
@@ -23,34 +23,132 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnOef1 = new System.Windows.Forms.Button();
+            this.btnOef2 = new System.Windows.Forms.Button();
+            this.btnOef3 = new System.Windows.Forms.Button();
+            this.btnOef4 = new System.Windows.Forms.Button();
+            this.btnOef5 = new System.Windows.Forms.Button();
+            this.btnOef6 = new System.Windows.Forms.Button();
+            this.btnOef7 = new System.Windows.Forms.Button();
+            this.btnOef8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // btnOef1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(220, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(445, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nog aan te vullen wanneer mevrouw stuurt welke oefeningen we wel en niet moeten k" +
-    "unnen";
+            this.btnOef1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef1.ForeColor = System.Drawing.Color.Red;
+            this.btnOef1.Location = new System.Drawing.Point(12, 12);
+            this.btnOef1.Name = "btnOef1";
+            this.btnOef1.Size = new System.Drawing.Size(182, 74);
+            this.btnOef1.TabIndex = 3;
+            this.btnOef1.Text = "Examenscore";
+            this.btnOef1.UseVisualStyleBackColor = true;
+            // 
+            // btnOef2
+            // 
+            this.btnOef2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef2.ForeColor = System.Drawing.Color.Red;
+            this.btnOef2.Location = new System.Drawing.Point(200, 12);
+            this.btnOef2.Name = "btnOef2";
+            this.btnOef2.Size = new System.Drawing.Size(182, 74);
+            this.btnOef2.TabIndex = 4;
+            this.btnOef2.Text = "Natuurlijke Getallen";
+            this.btnOef2.UseVisualStyleBackColor = true;
+            // 
+            // btnOef3
+            // 
+            this.btnOef3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef3.ForeColor = System.Drawing.Color.Red;
+            this.btnOef3.Location = new System.Drawing.Point(388, 12);
+            this.btnOef3.Name = "btnOef3";
+            this.btnOef3.Size = new System.Drawing.Size(182, 74);
+            this.btnOef3.TabIndex = 5;
+            this.btnOef3.Text = "Sterren";
+            this.btnOef3.UseVisualStyleBackColor = true;
+            // 
+            // btnOef4
+            // 
+            this.btnOef4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef4.ForeColor = System.Drawing.Color.Red;
+            this.btnOef4.Location = new System.Drawing.Point(576, 12);
+            this.btnOef4.Name = "btnOef4";
+            this.btnOef4.Size = new System.Drawing.Size(182, 74);
+            this.btnOef4.TabIndex = 6;
+            this.btnOef4.Text = "Grootste";
+            this.btnOef4.UseVisualStyleBackColor = true;
+            // 
+            // btnOef5
+            // 
+            this.btnOef5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef5.ForeColor = System.Drawing.Color.Red;
+            this.btnOef5.Location = new System.Drawing.Point(12, 92);
+            this.btnOef5.Name = "btnOef5";
+            this.btnOef5.Size = new System.Drawing.Size(182, 74);
+            this.btnOef5.TabIndex = 7;
+            this.btnOef5.Text = "Tafels";
+            this.btnOef5.UseVisualStyleBackColor = true;
+            // 
+            // btnOef6
+            // 
+            this.btnOef6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef6.ForeColor = System.Drawing.Color.Red;
+            this.btnOef6.Location = new System.Drawing.Point(200, 92);
+            this.btnOef6.Name = "btnOef6";
+            this.btnOef6.Size = new System.Drawing.Size(182, 74);
+            this.btnOef6.TabIndex = 8;
+            this.btnOef6.Text = "10 maal afdrukken";
+            this.btnOef6.UseVisualStyleBackColor = true;
+            // 
+            // btnOef7
+            // 
+            this.btnOef7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef7.ForeColor = System.Drawing.Color.Red;
+            this.btnOef7.Location = new System.Drawing.Point(388, 92);
+            this.btnOef7.Name = "btnOef7";
+            this.btnOef7.Size = new System.Drawing.Size(182, 74);
+            this.btnOef7.TabIndex = 9;
+            this.btnOef7.Text = "Fibonacci";
+            this.btnOef7.UseVisualStyleBackColor = true;
+            // 
+            // btnOef8
+            // 
+            this.btnOef8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOef8.ForeColor = System.Drawing.Color.Red;
+            this.btnOef8.Location = new System.Drawing.Point(576, 92);
+            this.btnOef8.Name = "btnOef8";
+            this.btnOef8.Size = new System.Drawing.Size(182, 74);
+            this.btnOef8.TabIndex = 10;
+            this.btnOef8.Text = "Tekenen";
+            this.btnOef8.UseVisualStyleBackColor = true;
             // 
             // frmIteraties
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(767, 175);
+            this.Controls.Add(this.btnOef8);
+            this.Controls.Add(this.btnOef7);
+            this.Controls.Add(this.btnOef6);
+            this.Controls.Add(this.btnOef5);
+            this.Controls.Add(this.btnOef4);
+            this.Controls.Add(this.btnOef3);
+            this.Controls.Add(this.btnOef2);
+            this.Controls.Add(this.btnOef1);
             this.Name = "frmIteraties";
             this.Text = "Iteraties";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOef1;
+        private System.Windows.Forms.Button btnOef2;
+        private System.Windows.Forms.Button btnOef3;
+        private System.Windows.Forms.Button btnOef4;
+        private System.Windows.Forms.Button btnOef5;
+        private System.Windows.Forms.Button btnOef6;
+        private System.Windows.Forms.Button btnOef7;
+        private System.Windows.Forms.Button btnOef8;
     }
 }
